81e431c7ce46528467fe72532d7f0baa4e2cca01
ravener <ravener.anime@gmail.com>
2021-01-30 09:09:24 +0430

    Add more scopes

--- discord.go
+++ discord.go
@@ -14,10 +14,19 @@ const (
   ScopeConnections = "connections"
   ScopeGroupDMJoin = "gdm.join"
   ScopeMessagesRead = "messages.read"
-  ScopeRPC = "rpc"
-  ScopeRPCAPI = "rpc.api"
-  ScopeRPCNotificationsRead = "rpc.notifications.read"
+  ScopeRPC = "rpc" // Whitelist only
+  ScopeRPCAPI = "rpc.api" // Whitelist only
+  ScopeRPCNotificationsRead = "rpc.notifications.read" // Whitelist only
   ScopeWebhookIncoming = "webhook.Incoming"
+	ScopeApplicationsBuildsUpload = "applications.builds.upload" // Whitelist only
+	ScopeApplicationsBuildsRead = "applications.builds.read"
+	ScopeApplicationsStoreUpdate = "applications.store.update"
+	ScopeApplicationsEntitlements = "applications.entitlements"
+	ScopeRelationshipsRead = "relationships.read" // Whitelist only
+	ScopeActivitiesRead = "activities.read" // Whitelist only
+	ScopeActivitiesWrite = "activities.write" // Whitelist only
+	ScopeApplicationsCommands = "applications.commands"
+	ScopeApplicationsCommandsUpdate = "applications.commands.update"
 )
 
 // Endpoint is Discord's OAuth 2.0 endpoint.
